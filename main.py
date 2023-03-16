5140c13912f063f5354e875caae663f877a6c8ba
Oleg Vinokurov <olegvinokurov@hotmail.com>
2023-03-15 22:07:16 -0700

    Added score logic each user gets $1000 to start

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+import os
 import random
 
 class Card:
@@ -52,7 +54,7 @@ def print_hands(player_hand, dealer_hand, hide_dealer_card=True):
         print(f"Dealer: {dealer_hand} ({dealer_hand.value()})")
     print(f"Player: {player_hand} ({player_hand.value()})")
 
-def blackjack_game():
+def blackjack_game(player_name, player_money):
     deck = Deck()
     player_hand = Hand()
     dealer_hand = Hand()
@@ -70,7 +72,8 @@ def blackjack_game():
             print_hands(player_hand, dealer_hand)
             if player_hand.value() > 21:
                 print("Player busts! Dealer wins.")
-                return
+                player_money -= 25
+                break
         elif action == 's':
             break
 
@@ -82,16 +85,58 @@ def blackjack_game():
 
     if dealer_hand.value() > 21:
         print("Dealer busts! Player wins.")
+        player_money += 25
     elif dealer_hand.value() < player_hand.value():
         print("Player wins!")
+        player_money += 25
     elif dealer_hand.value() > player_hand.value():
         print("Dealer wins!")
+        player_money -= 25
     else:
         print("It's a tie!")
 
+    return player_money
+
+def save_score(player_name, player_money):
+    with open("scores.json", "r") as file:
+        scores = json.load(file)
+    scores[player_name] = player_money
+    with open("scores.json", "w") as file:
+        json.dump(scores, file)
+
+def create_scores_file():
+    with open("scores.json", "w") as file:
+        json.dump({}, file)
+
 if __name__ == "__main__":
+    if not os.path.isfile("scores.json"):
+        create_scores_file()
+
+    with open("scores.json", "r") as file:
+        scores = json.load(file)
+
     while True:
-        blackjack_game()
+        player_name = input("Enter your name: ")
+        if player_name in scores:
+            print("Welcome back, {}! Your current balance is ${}.".format(player_name, scores[player_name]))
+            player_money = scores[player_name]
+            break
+        else:
+            player_money = 1000
+            scores[player_name] = player_money
+            save_score(player_name, player_money)
+            print("Welcome, {}! You have been given $1000 to start your game.".format(player_name))
+            break
+
+    while True:
+        if player_money < 25:
+            print("You don't have enough money to play another hand.")
+            break
+        print("Current balance: ${}".format(player_money))
+        player_money = blackjack_game(player_name, player_money)
+        save_score(player_name, player_money)
         play_again = input("Do you want to play again? (y/n): ").lower()
         if play_again != 'y':
             break
+
+    print("Thanks for playing! Your final balance is ${}.".format(player_money))
